1b7478dceaec77ff57932993540b255c98a22022
arunkumarmg1 <Asunuwar542@gmail.com>
2022-09-16 21:37:47 +0530

    testing

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "aws_sagemaker_notebook_url" {
+/* output "aws_sagemaker_notebook_url" {
     value = aws_sagemaker_notebook_instance.sgmkr.url
+} */
+output "zone" {
+    value = "${google_compute_instance.default.*.zone}"
 }
